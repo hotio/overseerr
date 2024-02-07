6775fd070912d61f2504fa1d0c7dcfb2daff4213
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 08:58:39 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/develop" | jq -r .sha)
-[[ -z ${version} ]] && exit 0
-message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/${version}" | jq -r .commit.message)
-if [[ ${message} == *"[skip ci]"* ]]; then
-    exit 0
-fi
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/sct/overseerr/compare/${old_version}...${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+set -e
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/develop" | jq -re .sha)
+message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/${version}" | jq -re .commit.message)
+[[ ${message} == *"[skip ci]"* ]] && exit 0
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    '.version = $version' <<< "${json}" | tee VERSION.json
