b69c652149360a85520b477917c6da76cfeec48a
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-08-23 18:18:50 +0200

    don't filter commits

--- update-versions.sh
+++ update-versions.sh
@@ -2,8 +2,8 @@
 version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/develop" | jq -re .sha) || exit 1
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
-message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/${version}" | jq -re .commit.message) || exit 1
-[[ ${message} == *"[skip ci]"* ]] && exit 0
+#message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/${version}" | jq -re .commit.message) || exit 1
+#[[ ${message} == *"[skip ci]"* ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
