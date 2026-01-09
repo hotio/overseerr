cc505adb388b05a77639328f272e4981c3b7afa8
mrhotio <26902309+mrhotio@users.noreply.github.com>
2026-01-09 08:56:39 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/develop" | jq -re .sha) || exit 1
-[[ -z ${version} ]] && exit 0
-[[ ${version} == null ]] && exit 0
-#message=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/sct/overseerr/commits/${version}" | jq -re .commit.message) || exit 1
-#[[ ${message} == *"[skip ci]"* ]] && exit 0
+set -exuo pipefail
+
+version=$(curl -fsSL "https://api.github.com/repos/sct/overseerr/commits/develop" | jq -re .sha)
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
